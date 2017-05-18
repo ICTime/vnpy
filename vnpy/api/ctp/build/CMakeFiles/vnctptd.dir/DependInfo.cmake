68c696950c61d770ce136296460584f7c2ececcc
ICTime <yubingem@gmail.com>
2017-05-18 10:45:15 +0800

    build ctp on aliyun

---
+++ vnpy/api/ctp/build/CMakeFiles/vnctptd.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ictime/vnpy/vnpy/api/ctp/vnctptd/vnctptd/vnctptd.cpp" "/home/ictime/vnpy/vnpy/api/ctp/build/CMakeFiles/vnctptd.dir/vnctptd/vnctptd/vnctptd.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS
+  "BUILD_CTP_MD"
+  "BUILD_CTP_TD"
+  "USE_64BITS"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/lib/ctpapi/x64_linux"
+  "../CTP_MD_PATH"
+  "../CTP_TD_PATH"
+  "/usr/include/python2.7"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
